77c3684360dd93c2426ee220ff948d262b213c59
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-12-04 16:08:00 -0300

    Update desafio92.py

--- Guanabara/desafio92.py
+++ Guanabara/desafio92.py
@@ -1,11 +1,13 @@
+from datetime import datetime
+
 dados = dict()
 dados['nome'] = str(input('Nome: '))
-dados['Idade'] = 2019 - int(input('Ano de Nascimento: '))
+dados['Idade'] = datetime.now().year - int(input('Ano de Nascimento: '))
 dados['cpts'] = int(input('Carteira de trabalho (0 não tem): '))
 if dados['cpts'] > 0:
     dados['contratacao'] = int(input('Ano de contratação: '))
     dados['salario'] = float(input('salario: '))
-    dados['aposentadoria'] = 32 - (2019 - (dados['contratacao'] + dados['Idade']))
+    dados['aposentadoria'] = dados['Idade'] + ((dados['contratacao'] + 35) -datetime.now().year)
 
 for k, v in dados.items():
     print(f'{k} tem o valor {v}')

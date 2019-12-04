b6a851f7cbdd148c5a2914bd5be6ef19f885738a
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-12-04 16:28:47 -0300

    Create desafio93.py

---
+++ Guanabara/desafio93.py
@@ -0,0 +1,20 @@
+jogador = dict()
+
+jogador['nome'] = str(input('Nome do Jogador: '))
+partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+gols = list()
+for i in range(0,partidas):
+    gols.append(int(input(f"Quantos gols na partida {i+1}: ")))
+jogador['gols'] = gols
+jogador['total'] = sum(gols)
+print('-='*30)
+print(jogador)
+print('-='*30)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-='*30)
+print(f'o jogador {jogador["nome"]} jogou {partidas} partidas')
+for i, v in enumerate(jogador['gols']):
+    print(f'Na partirda {i+1}, fez {v} gols')
+print('-='*30)
+print(f'foi um total de {jogador["total"]} gols')

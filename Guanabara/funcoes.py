7ed93efb5c1b92c07023fa0c01cecfc60faca1e3
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-12-09 18:06:13 -0300

    aula funções

--- Guanabara/funcoes.py
+++ Guanabara/funcoes.py
@@ -5,3 +5,26 @@ def mostrarlinha(texto):
 
 
 mostrarlinha("nidio")
+
+
+def soma(a, b):
+    print(f'A = {a} e B = {b}')
+    print(a + b)
+
+
+soma(b=5, a=6)
+
+
+def contador(*num):
+    print(sum(num))
+
+
+contador(1, 2, 3)
+contador(1, 2, 3, 4, 5, 6, 7)
+
+def listaa(lst):
+    print(lst)
+
+lsita = [0, 1, 2, 3, 4]
+
+listaa(lsita)

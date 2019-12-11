58ee7db9e4f56ce84fed2ec9addb9835e61106a2
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-12-11 16:05:51 -0300

    desafio 96 e 97

---
+++ Guanabara/desafio96.py
@@ -0,0 +1,5 @@
+def area(largura, altura):
+    print(f'A area do terreno {largura} x {altura} é {largura * altura}m²')
+
+
+area(4.5, 8)
---
+++ Guanabara/desafio97.py
@@ -0,0 +1,7 @@
+def escreva(mensagem):
+    print('~' * (len(mensagem) + 4))
+    print(" ", mensagem)
+    print('~' * (len(mensagem) + 4))
+
+
+escreva('olá, mundo!')

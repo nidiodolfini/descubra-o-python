a245a156390ec9e1cb6e47b3595f886050c5082a
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-12-15 12:00:44 -0300

    aula 2 funções e desafios

--- Guanabara/funcoes.py
+++ Guanabara/funcoes.py
@@ -1,34 +1,56 @@
-def mostrarlinha(texto):
-    print('-=' * 30)
-    print(texto)
-    print('=-' * 30)
+# def mostrarlinha(texto):
+#     print('-=' * 30)
+#     print(texto)
+#     print('=-' * 30)
+#
+#
+# mostrarlinha("nidio")
+#
+#
+# def soma(a=0, b=0):
+#     print(f'A = {a} e B = {b}')
+#     print(a + b)
+#
+#
+# soma(b=5, a=6)
+# soma(7, 3)
+# soma()
+#
+#
+# def contador(*num):
+#     """
+#     -faz a soma de N numeros
+#
+#     """
+#
+#     print(sum(num))
+#
+#
+# help(contador)
+#
+# contador(1, 2, 3)
+# contador(1, 2, 3, 4, 5, 6, 7)
+#
+#
+# def listaa(lst):
+#     print(lst)
+#
+#
+# lsita = [0, 1, 2, 3, 4]
+#
+# listaa(lsita)
+#
+# help(input())
+#
+
+def teste(b):
+    global a
+    a = 8
+    b += 4
+    c = 2
+    print(f'a vale {a}')
+    print(f'B vale {b}')
 
 
-mostrarlinha("nidio")
-
-
-def soma(a, b):
-    print(f'A = {a} e B = {b}')
-    print(a + b)
-
-
-soma(b=5, a=6)
-
-
-def contador(*num):
-    print(sum(num))
-
-
-contador(1, 2, 3)
-contador(1, 2, 3, 4, 5, 6, 7)
-
-
-def listaa(lst):
-    print(lst)
-
-
-lsita = [0, 1, 2, 3, 4]
-
-listaa(lsita)
-
-help(input())
+a = 5
+teste(a)

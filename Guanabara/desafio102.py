1b7bdcfb15f09928992835379e247462822db391
Nídio Dolfini <nidiosdolfini@gmail.com>
2019-12-22 20:17:26 -0300

    Update desafio102.py

--- Guanabara/desafio102.py
+++ Guanabara/desafio102.py
@@ -1,7 +1,16 @@
-def fatorial(num = 1, show = True):
+def fatorial(num=1, show=True):
+    if show:
+        fat = num
+        for i in range(1, num + 1):
+            if fat != 1:
+                print(f'{fat}', end=' x ')
+            else:
+                print(f'{fat}', end=' = ')
+
+            fat -= 1
     for i in range(1, num):
         num *= i
-    print(num)
+    print(f'{num}')
 
 
-fatorial(0, True)
+fatorial(5, True)

18836e2498ccd0e332efbed9c7042f09295b9171
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-01-02 15:30:06 -0300

    Update teste.py

--- Codenation/teste.py
+++ Codenation/teste.py
@@ -1,2 +1,7 @@
-def add_binary(a,b):
-    soma = a + b
+ano = 2019
+resto = ano % 4
+print(f'resto {resto}')
+resto100 = ano % 100
+print(f'resto 100 {resto100}')
+resto400 = ano % 400
+print(f'resto 400 {resto400}')

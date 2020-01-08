1a0e0442231ecb852c65d5b703b74597546323cc
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-01-08 17:47:34 -0300

    Update teste.py

--- datascienceacademy/teste.py
+++ datascienceacademy/teste.py
@@ -1,3 +1,15 @@
 x = 'Data Science Academy'
 
 print(x[2:])
+
+lista = list(range(1, 10))
+
+print(lista)
+
+nome = 'Nidio Dolfini'
+nome1 = nome.split(' ')
+print(nome1)
+
+nome3 = ''.join(nome1)
+
+print(nome3)

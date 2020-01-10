fbc0022a6b231d1be4df604507bb688254308c5b
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-01-10 11:13:03 -0300

    Update calcular_dias.py

--- teste/calcular_dias.py
+++ teste/calcular_dias.py
@@ -1,18 +1,18 @@
 from datetime import date, timedelta
 
 
-def QuantosDiasFaltam(ano, mes, dia):
+def QuantosDiasFaltam(ano, mes, dia, quantosDias):
     hoje = date.today()
     dataProcurada = date(ano, mes, dia)
-    delta = timedelta(days=100)
+    delta = timedelta(days=quantosDias)
     quandotermina = dataProcurada + delta
-    quantosDias = hoje - quandotermina
+    quantosDiasParaTerminar = hoje - quandotermina
 
-    mensagemRetorno = "Faltam " + str(quantosDias).replace("days, 0:00:00",
-                                                           "") + " dias para a data " + quandotermina.strftime(
+    mensagemRetorno = "Faltam " + str(quantosDiasParaTerminar).replace("days, 0:00:00",
+                                                                       "") + " dias para a data " + quandotermina.strftime(
         "%d/%m/%y")
 
     print(mensagemRetorno)
 
 
-QuantosDiasFaltam(2019, 12, 2)
+QuantosDiasFaltam(2019, 12, 2, 100)

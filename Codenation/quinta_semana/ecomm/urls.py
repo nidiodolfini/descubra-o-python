96a129f0af587d6f6396af53dda0f1131745e688
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-01-21 16:28:05 -0300

    arquivos da aula de Simple List Products quinta seman

--- Codenation/quinta_semana/ecomm/urls.py
+++ Codenation/quinta_semana/ecomm/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from products.views import list_products
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', list_products)
 ]
--- Codenation/quinta_semana/products/views.py
+++ Codenation/quinta_semana/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
+
+def list_products(request):
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+
+    }
+    return render(request, 'products/list.html', context=context)

9de501840ace2b6156863226b6fdd2c8e55a3e6c
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-01-20 21:30:20 -0300

    created model of product

--- Codenation/quinta_semana/products/models.py
+++ Codenation/quinta_semana/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField('Nome', max_length=100)
+    description = models.TextField('Descrição')
+    price = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- Codenation/quinta_semana/products/tests.py
+++ Codenation/quinta_semana/products/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from .models import Product
+
 # Create your tests here.
+class ProductStrTestCase(TestCase):
+    def test_str_should_return_name(self):
+        product = Product.objects.create(
+            name='Teste Produto',
+            description='Teste Descrição',
+            price=10.5
+        )
+
+        self.assertEqual(str(product), 'Teste Produto')
+        

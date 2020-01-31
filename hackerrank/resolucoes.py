f8754a54c08ae1153ca3c21a7fb2a2b206c31cd6
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-01-31 10:38:49 -0300

    resolução dia um do hackerrank

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,3 +1,19 @@
-n = 5
-for i in range(n):
-    print(i**2)
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+i1 = 0
+d1 = 0
+s1 = ""
+# Read and save an integer, double, and String to your variables.
+i1 = int(input())
+d1 = float(input())
+s1 = input()
+# Print the sum of both integer variables on a new line.
+print(i + i1)
+# Print the sum of the double variables on a new line.
+print(d + d1)
+# Concatenate and print the String variables on a new line
+print(s + s1)
+# The 's' variable above should be printed first.

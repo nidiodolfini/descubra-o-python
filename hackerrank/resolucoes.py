328aa82cd0b2235f1eed39e70ef141772aa4685b
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-01 14:33:55 -0300

    Day 2: Operators

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,11 +1,5 @@
-n = 3
-def diagonalDifference(arr):
-    ans = 0
-    for i in range(n):
-        ans += arr[i][i] - arr[i][n - i - 1]
-    return abs(ans)
-arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
-print(diagonalDifference(arr))
+def solve(meal_cost, tip_percent, tax_percent):
+    meal_cost += ((meal_cost * tip_percent) / 100) + ((meal_cost * tax_percent) / 100)
+    return round(meal_cost)
 
-# for i in range(n, 0, -1):
-#     print(i)
+print(solve(12.0, 20, 8))

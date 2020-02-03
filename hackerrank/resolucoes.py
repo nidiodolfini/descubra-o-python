75f5f6416796a0d73c0aee43a668885b8ff23e4d
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-03 15:31:40 -0300

    Update resolucoes.py

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,4 +1,7 @@
-def solve(s):
-    texto = s
-    texto.split()
-solve('nidio dos santos dolfini')
+a = int(input())
+b = int(input())
+div1 = a // b
+div2 = a % b
+print(div1)
+print(div2)
+print(f'({div1}, {div2})')

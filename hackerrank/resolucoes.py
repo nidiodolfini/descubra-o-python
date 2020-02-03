0710fa1ed3e222a8c991c339f67de23536c7175a
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-03 11:16:41 -0300

    What's Your Name?

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,45 +1,4 @@
+def print_full_name(a, b):
+    print(f"Hello {a} {b}! You just delved into python.")
 
-class Person:
-    age = 0
-
-    def __init__(self, initialAge):
-        if initialAge < 0:
-            print('Age is not valid, setting age to 0.')
-            self.age = 0
-        else:
-            self.age = initialAge
-
-    def amIOld(self):
-        if self.age < 13:
-            print('You are young.')
-        elif 13 <= self.age < 18:
-            print('You are a teenager.')
-        else:
-            print('You are old.')
-
-    def yearPasses(self):
-        self.age += 1
-
-
-t = int(input())
-for i in range(0, t):
-    age = int(input())
-    p = Person(age)
-    p.amIOld()
-    for j in range(0, 3):
-        p.yearPasses()
-    p.amIOld()
-    print("")
-
-N = int(input())
-
-if N % 2 != 0:
-    print('Weird')
-elif N % 2 == 0:
-    if 2 <= N < 5:
-        print('Not Weird')
-    elif 6 <= N <= 20:
-        print('Weird')
-    elif N > 20:
-        print('Not Weird')
-
+print_full_name('Nidio', 'Dolfini')

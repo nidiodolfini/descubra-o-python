f2a71dc5a31a006b272c8c7a2dfcef0eb4c5e63f
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-04 16:49:00 -0300

    Update resolucoes.js

--- hackerrank/resolucoes.js
+++ hackerrank/resolucoes.js
@@ -1,14 +1,1 @@
-function performOperation(secondInteger, secondDecimal, secondString) {
-    // Declare a variable named 'firstInteger' and initialize with integer value 4.
-    const firstInteger = 4;
-    console.log(firstInteger + int(secondInteger));
-    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
-    const firstDecimal = 4.0;
-    console.log(firstDecimal + float(secondDecimal));
-    
-    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
-    const firstString = 'HackerRank ';
-    console.log(firstString + secondString);
-}
-
-performOperation("10", "10", "meu pau" );
+console.log("Nidio")

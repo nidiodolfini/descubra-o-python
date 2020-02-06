a917ccf78ae40be203e054456584360e207feae4
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-06 11:41:13 -0300

    Day 1: Functions

--- hackerrank/resolucoes.js
+++ hackerrank/resolucoes.js
@@ -2,7 +2,7 @@ function fatorial(n) {
     var fat = 0
     if (n > 1) {
         for (i = 0; i < n; i++){
-            fat = fat * n 
+            fat += fat * n 
         }
         return fat;
     }

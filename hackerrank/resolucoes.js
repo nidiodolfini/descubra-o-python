fd9120f12652f3072a7c4a21e03b826560e1c2b2
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-06 11:40:53 -0300

    Day 1: Functions

--- hackerrank/resolucoes.js
+++ hackerrank/resolucoes.js
@@ -1,26 +1,13 @@
-function getArea(length, width) {
-    let area;
-    // Write your code here
-    area = length * width
-    
-    return area;
+function fatorial(n) {
+    var fat = 0
+    if (n > 1) {
+        for (i = 0; i < n; i++){
+            fat = fat * n 
+        }
+        return fat;
+    }
+    return 1;
+
 }
 
-/**
-*   Calculate the perimeter of a rectangle.
-*	
-*	length: The length of the rectangle.
-*   width: The width of the rectangle.
-*   
-*	Return a number denoting the perimeter of a rectangle.
-**/
-function getPerimeter(length, width) {
-    let perimeter;
-    
-    perimeter = 2 * (length + width)
-    
-    return perimeter;
-}
-
-console.log(getArea(3, 4.5))
-console.log(getPerimeter(3, 4.5))
+console.log(fatorial(4))

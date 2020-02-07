4992d411e38587716f5a1cc4e3a6ddaa20d7adbc
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-07 14:39:21 -0300

    Day 8: Dictionaries and Maps

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,6 +1,34 @@
 n = int(input())
+contato = {}
+contatos = list()
+nome = list()
+for c in range(0,n):
+    arr = list(input().rstrip().split())
+    contato['name'] = arr[0]
+    contato['phone'] = arr[1]
+    contatos.append(contato.copy())
+for c in range(0,n):
+    nome += list((input().rstrip().split()))
+for i in range(0,n):
+    pesquisa = nome[i]
+    if pesquisa in contatos[i]['name']:
+        print(f'{contatos[i]["name"]}={contatos[i]["phone"]}')
+    else:
+        print('Not found')
+import sys
 
-arr = list(map(int, input().rstrip().split()))
+# Read input and assemble Phone Book
+n = int(input())
+phoneBook = {}
+for i in range(n):
+    contact = input().split(' ')
+    phoneBook[contact[0]] = contact[1]
 
-for i in range(n, 0, -1):
-    print(arr[i-1])
+# Process Queries
+lines = sys.stdin.readlines()
+for i in lines:
+    name = i.strip()
+    if name in phoneBook:
+        print(name + '=' + str( phoneBook[name] ))
+    else:
+        print('Not found')

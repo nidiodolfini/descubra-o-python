7107a21f0d93356463244bfb54e50ab3fb3e73aa
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-08 17:15:03 -0300

    Day 3: Try, Catch, and Finally

--- hackerrank/resolucoes.js
+++ hackerrank/resolucoes.js
@@ -1,21 +1,23 @@
-function getSecondLargest(nums) {
-    // Complete the function
-    let largest = nums[0];
-    let secondLargest = nums[0];
-    
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] > largest) {
-            secondLargest = largest;
-            largest = nums[i];
-            continue;
-        }
-
-        if ((nums[i] > secondLargest) && (nums[i] < largest)) {
-            secondLargest = nums[i];
-        }
+function reverseString(s) {
+    try {
+        var splitString = s.split(''); //divide a palavra em um array letras 
+        var reverseArray = splitString.reverse(); //inverter o array 
+        var joinArray = reverseArray.join(''); // junta o array de palavras
+        return joinArray;
     }
+    catch (e) {
+        console.log("s.split is not a function");
+        console.log(s)
+    }
+        
     
-    return secondLargest;
 }
 
-console.log(getSecondLargest('2 3 6 6 5'))
+reverseString(Number(1234));
+console.log(reverseString('1234'));
+
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+reverseString("hello");

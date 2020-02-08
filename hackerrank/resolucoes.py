cae75a117641b56b6fa28e42f05e78b526ffc658
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-08 14:44:17 -0300

    Day 9: Recursion 3

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,34 +1,7 @@
-n = int(input())
-contato = {}
-contatos = list()
-nome = list()
-for c in range(0,n):
-    arr = list(input().rstrip().split())
-    contato['name'] = arr[0]
-    contato['phone'] = arr[1]
-    contatos.append(contato.copy())
-for c in range(0,n):
-    nome += list((input().rstrip().split()))
-for i in range(0,n):
-    pesquisa = nome[i]
-    if pesquisa in contatos[i]['name']:
-        print(f'{contatos[i]["name"]}={contatos[i]["phone"]}')
+def fatorial(n):
+    if n == 0 or n == 1:
+        return 1
     else:
-        print('Not found')
+        return n * fatorial(n - 1)
 
-import sys
-# Read input and assemble Phone Book
-n = int(input())
-phoneBook = {}
-for i in range(n):
-    contact = input().split(' ')
-    phoneBook[contact[0]] = contact[1]
-
-# Process Queries
-lines = sys.stdin.readlines()
-for i in lines:
-    name = i.strip()
-    if name in phoneBook:
-        print(name + '=' + str( phoneBook[name] ))
-    else:
-        print('Not found')
+print(fatorial(5))

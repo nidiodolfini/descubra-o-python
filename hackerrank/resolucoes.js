91204602c9f2a2cf670fa7e2dc650a2a06f9ae47
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-08 15:12:24 -0300

    Day 3: Arrays

--- hackerrank/resolucoes.js
+++ hackerrank/resolucoes.js
@@ -1,54 +1,22 @@
-function vowelsAndConsonants(s) {
-    var vogais = '';
-    var consoantes = '';
-  for (var i = 0; i < s.length; i++){
-    if('aeiou'.includes(s[i])){
-        vogais += s[i]
-    }else {
-        consoantes += s[i]
-    }
+function getSecondLargest(nums) {
+    // Complete the function
+    let largest = nums[0];
+    let secondLargest = nums[0];
     
-  }
-  var teste = vogais;
-  teste += consoantes;
-  for (i = 0; i < teste.length; i++){
-    console.log(teste[i])
-  }
-}
-
-vowelsAndConsonants('javascriptloops')
-
-function vowelsAndConsonants(s) {
-    let vowels = '';
-    let consonants = '';
-
-    // For each letter in string s
-    for (const letter of s) {
-        // Separate vowels and consonants
-        if (letter == 'a' ||
-            letter == 'e' ||
-            letter == 'i' ||
-            letter == 'o' ||
-            letter == 'u'
-           ) {
-            // Append vowel to vowel string
-            vowels += letter;
-        } else {
-            // Append consonant to consonant string
-            consonants += letter;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+            continue;
+        }
+        console.log("s",secondLargest)
+        console.log('l',largest)
+        if ((nums[i] > secondLargest) && (nums[i] < largest)) {
+            secondLargest = nums[i];
         }
     }
     
-    // Print each vowel
-    // This loop iterates over each character in the vowels string in order
-    for (let i = 0; i < vowels.length; i++) {
-        console.log(vowels.charAt(i));
-    }
-    
-    // Print each consonant
-    // This loop iterates over each letter in an array consisting of the consonants
-    for (const letter of consonants.split('')) {
-        console.log(letter);
-    }
+    return secondLargest;
 }
-vowelsAndConsonants('nidio')
+
+console.log(getSecondLargest('2 3 6 6 5'))

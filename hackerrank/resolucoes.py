28d5f159b0973214d6efe562e6a555d2c4d6e613
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-10 10:38:27 -0300

    Day 11: 2D Arrays

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,9 +1,13 @@
-from itertools import groupby
+arr = []
 
+for _ in range(6):
+    arr.append(list(map(int, input().rstrip().split())))
 
-def converter(n):
-    print(max([len(list(g)) for k, g in groupby(bin(n))]))
-    # return bin(n)[2:]
+res = []
 
+for x in range(0, 4):
+    for y in range(0, 4):
+        s = sum(arr[x][y:y + 3]) + arr[x + 1][y + 1] + sum(arr[x + 2][y:y + 3])
+        res.append(s)
 
-converter(13)
+print(max(res))

a99ffb20e96382e3a677a9d22204335f00adc1fc
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-11 11:46:29 -0300

    Day 12: Inheritance

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,8 +1,63 @@
-import pyqrcode
-from pyqrcode import QRCode
+from pip._vendor.distlib.compat import raw_input
 
-s = 'https://nidio.dev'
 
-url = pyqrcode.create(s)
+class Person:
+    def __init__(self, firstName, lastName, idNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.idNumber = idNumber
 
-url.svg('myqr.svg', scale=8)
+    def printPerson(self):
+        print
+        "Name:", self.lastName + ",", self.firstName
+        print
+        "ID:", self.idNumber
+
+
+class Student(Person):
+    # Class Constructor
+    #
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    class Student(Person):
+        def __init__(self, firstName, lastName, idNumber, scores):
+            Person.__init__(self, firstName, lastName, idNumber)
+            self.scores = scores
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    #
+    # Write your function here
+    def calculate(self):
+        sum = 0
+        for x in range(0, len(scores)):
+            sum = sum + scores[x]
+        avg = sum / len(scores)
+        if avg >= 90:
+            return 'O'
+        elif avg >= 80:
+            return 'E'
+        elif avg >= 70:
+            return 'A'
+        elif avg >= 55:
+            return 'P'
+        elif avg >= 40:
+            return 'D'
+        else:
+            return 'T'
+
+
+line = raw_input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(raw_input())  # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())

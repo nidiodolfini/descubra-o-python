c1cda660fb377be3acaf5a1ffdbaf90805c25096
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-12 09:52:19 -0300

    upodate

--- hackerrank/resolucoes.js
+++ hackerrank/resolucoes.js
@@ -1,22 +1,18 @@
-/*
- * Implement a Polygon class with the following properties:
- * 1. A constructor that takes an array of integer side lengths.
- * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
- */
+Declare your class here.
+    /**
+    *   Class Constructor
+    *
+    *   @param title The book's title.
+    *   @param author The book's author.
+    *   @param price The book's price.
+    **/
+    // Write your constructor here
 
- class Polygon {
-    constructor(sides){
-        this.sides = sides;
-     }
-    perimeter() {
-        return this.sides.reduce(function add(a,b){return a+b;})
-    }
- }
+    /**
+    *   Method Name: display
+    *
+    *   Print the title, author, and price in the specified format.
+    **/
+    // Write your method here
 
-const rectangle = new Polygon([10, 20, 10, 20]);
-const square = new Polygon([10, 10, 10, 10]);
-const pentagon = new Polygon([10, 20, 30, 40, 43]);
-
-console.log(rectangle.perimeter());
-console.log(square.perimeter());
-console.log(pentagon.perimeter());
+End class

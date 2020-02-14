5c2caff6ad9998e04b40517550e3bd8a210d0e1b
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-14 18:59:51 -0300

    Day 15: Linked List

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,21 +1,17 @@
-class Difference:
-    def __init__(self, a):
-        self.__elements = a
-        self.maximumDifference = self.computeDifference()
+def insert(self,head,data):
+    if (head == None):
+        head = Node(data)
+    elif (head.next == None):
+        head.next = Node(data)
+    else:
+        self.insert(head.next, data)
+    return head
 
-	# Add your code here
-
-    def computeDifference(self):
-        self.__elements.sort()
-        return self.__elements[-1] - self.__elements[0]
-
-
-# End of Difference class
-
-_ = input()
-a = [int(e) for e in input().split(' ')]
-
-d = Difference(a)
-d.computeDifference()
-
-print(d.maximumDifference)
+#
+# The function gets called with the whole linked list.
+#     If the list is empty,
+#     	insert the element as the head.
+#     Otherwise if there is no element after the head,
+#     	put the data after the head.
+#     Otherwise,
+#     	call the function with all of the elements except for the current head

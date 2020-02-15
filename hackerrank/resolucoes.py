5e322cb57d03216b64910395c6fc7f7412551b72
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-14 21:42:11 -0300

    Staircase

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,23 +1,3 @@
-def plusMinus(arr):
-    negative = 0
-    positive = 0
-    zero = 0
-    for i in range(n):
-        if arr[i] == 0:
-            zero += 1
-        elif arr[i] < 0:
-            negative += 1
-        else:
-            positive += 1
-    print(positive / n)
-    print(negative / n)
-    print(zero / n)
-
-
-n = int(input())
-
-arr = list(map(int, input().rstrip().split()))
-
-plusMinus(arr)
-
-
+n = 6
+for i in range(1, n + 1):
+        print(str('#'*i).rjust(n)) #rjust joga tudo para a direita baseado na posição do n

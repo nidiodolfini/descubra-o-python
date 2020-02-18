a21d2b6d2c5931237f6cdd6ee644e5a2d8009b6d
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-18 15:05:05 -0300

    is_isogram
    solução codewars

--- codewars/testesAntesDoEnvio.py
+++ codewars/testesAntesDoEnvio.py
@@ -1,12 +1,22 @@
-def is_divisible(n,x,y):
-    if n % x == 0 and n % y == 0:
-        return True
-    else:
+# An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+# Implement a function that determines whether a string that contains only letters 
+# is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+def is_isogram(string):
+    cont = 0
+    word = string.lower()
+    for char in word:
+        if word.count(char) > 1:
+            cont += 1
+    if cont > 1 :
         return False
+    else:
+        return True
+
+def is_isogram(string):
+    return len(string) == len(set(string.lower()))
 
 
-print(is_divisible(3,3,4))
-print(is_divisible(12, 3, 4))
-print(is_divisible(8,3,4))
-print(is_divisible(48,3,4))
-
+print(is_isogram("Dermatoglyphics"))
+print(is_isogram("aba" ))
+print(is_isogram("nidio" ))

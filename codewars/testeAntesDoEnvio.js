95e8e98e71443e2e25e54218cfe024c7b08d53c1
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-18 15:25:07 -0300

    Convert number to reversed array of digits

---
+++ codewars/testeAntesDoEnvio.js
@@ -0,0 +1,7 @@
+function digitize(num) {
+    return num.toString().split('').reverse().map(Number);
+}
+//Convert number to reversed array of digits
+//You have to return the digits of this number within an array in reverse order.
+//Example:
+//348597 => [7,9,5,8,4,3]

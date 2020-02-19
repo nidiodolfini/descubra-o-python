f86b5d2dafd36b827019d7ded041d7ecce85cff0
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-19 14:24:28 -0300

    Update resolucoes.py

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,15 +1,24 @@
-n = int(input().strip())
-a = list(map(int, input().strip().split(' ')))
-# Write Your Code Here
+# n = int(input().strip())
+# a = list(map(int, input().strip().split(' ')))
+# # Write Your Code Here
+#
+# numSwaps = 0
+# for i in range(len(a)-1,0,-1):
+#     for j in range(i):
+#         if a[j] > a[j+1]:
+#             temp = a[j]
+#             a[j] = a[j+1]
+#             a[j+1] = temp
+#             numSwaps += 1
+# print('Array sorted in',numSwaps,'swaps')
+# print('First Element:',a[0])
+# print('Last Element:',a[-1])
+#
 
-numSwaps = 0
-for i in range(len(a)-1,0,-1):
-    for j in range(i):
-        if a[j] > a[j+1]:
-            temp = a[j]
-            a[j] = a[j+1]
-            a[j+1] = temp
-            numSwaps += 1
-print('Array sorted in',numSwaps,'swaps')
-print('First Element:',a[0])
-print('Last Element:',a[-1])
+print('The word "Ciao" means "hello" in Italian.')
+
+print("hello")
+print("")
+print()
+
+print("bye")

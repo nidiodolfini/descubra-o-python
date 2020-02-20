166302630067c3de49b459804ad97931ebab5e54
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-20 14:31:07 -0300

    CamelCase

--- hackerrank/resolucoes.py
+++ hackerrank/resolucoes.py
@@ -1,24 +1,9 @@
-# n = int(input().strip())
-# a = list(map(int, input().strip().split(' ')))
-# # Write Your Code Here
-#
-# numSwaps = 0
-# for i in range(len(a)-1,0,-1):
-#     for j in range(i):
-#         if a[j] > a[j+1]:
-#             temp = a[j]
-#             a[j] = a[j+1]
-#             a[j+1] = temp
-#             numSwaps += 1
-# print('Array sorted in',numSwaps,'swaps')
-# print('First Element:',a[0])
-# print('Last Element:',a[-1])
-#
-a = "Tuesday"
-b = 10
-a = b
-b = a
-a = 10.0
+def camelcase(s):
+    contWord = 1
+    for i in s:
+        if i.isupper():
+            contWord += 1
+    print(contWord)
 
 
-print(type(10/0))
+camelcase('saveChangesInTheEditor')

af4156c78cb824f1280464c9c825ad9ed7f2c402
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-02-21 09:54:08 -0300

    Day 22: Binary Search Trees

--- jetbrains_academy/annotations.py
+++ jetbrains_academy/annotations.py
@@ -1,3 +1,36 @@
+amount = 1000
+interest_rate = 5
+years = 1
+
+income = ((amount * interest_rate) / 100) * years
+
+print(income)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # To sum up, there is a list of priorities for all considered operations:
 #
 # parentheses ()
@@ -5,21 +38,21 @@
 # unary minus - ()
 # multiplication, division, and remainder * / %
 # addition and subtraction + -
-from math import ceil
-# Read an integer:
-a = int(input())
-b = int(input())
-c = int(input())
-# Math
-
-classa=ceil(a/2)
-classb=ceil(b/2)
-classc=ceil(c/2)
-classall=(classa+classb+classc)
-
-print("%s" % classall)
-
-a=int(input())
-b=int(input())
-c=int(input())
-print((a//2)+(b//2)+(c//2)+(a%2)+(b%2)+(c%2))
+# from math import ceil
+# # Read an integer:
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# # Math
+#
+# classa=ceil(a/2)
+# classb=ceil(b/2)
+# classc=ceil(c/2)
+# classall=(classa+classb+classc)
+#
+# print("%s" % classall)
+#
+# a=int(input())
+# b=int(input())
+# c=int(input())
+# print((a//2)+(b//2)+(c//2)+(a%2)+(b%2)+(c%2))

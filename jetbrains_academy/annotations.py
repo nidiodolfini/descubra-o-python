105c60e132d593a9c6ede164f90d0625e34d4e6a
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-20 21:47:39 -0300

    # put your python code here

--- jetbrains_academy/annotations.py
+++ jetbrains_academy/annotations.py
@@ -5,3 +5,10 @@
 # unary minus - ()
 # multiplication, division, and remainder * / %
 # addition and subtraction + -
+duration_in_days = int(input())
+food_per_day = int(input())
+one_way_flight = int(input())
+hotel_cost = int(input())
+
+print((food_per_day * duration_in_days) + (hotel_cost * (duration_in_days - 1) + (one_way_flight * 2)))
+

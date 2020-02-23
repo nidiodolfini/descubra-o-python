ed47e82a781d2ea9131b2d384d267b30ed6468ab
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-02-23 13:26:00 -0300

    problem of day 23/02/20 Ticket
    Jetbrains problem of day 23/02/20 Ticket

--- jetbrains_academy/annotations.py
+++ jetbrains_academy/annotations.py
@@ -1,21 +1,43 @@
-from math import floor
-# 200 ml of water, 50 ml of milk, and 15 g
-water = int(input('Write how many ml of water the coffee machine has: '))
-milk = int(input('Write how many ml of milk the coffee machine has: '))
-coffee_beans = int(input('Write how many grams of coffee beans the coffee machine has: '))
-amount_of_coffee = int(input('Write how many cups of coffee you will need: '))
-water_necessary = floor(water / 200)
-milk_necessary = floor(milk / 50)
-coffee_beans_necessary = floor(coffee_beans / 15)
-smallest_item = min(water_necessary, milk_necessary, coffee_beans_necessary)
-if amount_of_coffee == smallest_item:
-    print('Yes, I can make that amount of coffee')
-elif amount_of_coffee < smallest_item:
-    sobra = smallest_item - amount_of_coffee
-    print(f"Yes, I can make that amount of coffee (and even {sobra} more than that)")
-elif amount_of_coffee > smallest_item:
-    sobra = -(smallest_item - amount_of_coffee)
-    print(f"No, I can make only {-(sobra - amount_of_coffee)} cups of coffee")
+
+# problem of day 23/02/20 Ticket
+# Save the input in this variable
+ticket = input()
+# Add up the digits for each half
+half1 = 0
+half2 = 0
+cont = 0
+for i in ticket:
+    if cont <= 2:
+        half1 += int(i)
+    else:
+        half2 += int(i)
+    cont += 1
+
+# Thanks to you, this code will work
+if half1 == half2:
+    print("Lucky")
+else:
+    print("Ordinary")
+
+
+# from math import floor
+# # 200 ml of water, 50 ml of milk, and 15 g
+# water = int(input('Write how many ml of water the coffee machine has: '))
+# milk = int(input('Write how many ml of milk the coffee machine has: '))
+# coffee_beans = int(input('Write how many grams of coffee beans the coffee machine has: '))
+# amount_of_coffee = int(input('Write how many cups of coffee you will need: '))
+# water_necessary = floor(water / 200)
+# milk_necessary = floor(milk / 50)
+# coffee_beans_necessary = floor(coffee_beans / 15)
+# smallest_item = min(water_necessary, milk_necessary, coffee_beans_necessary)
+# if amount_of_coffee == smallest_item:
+#     print('Yes, I can make that amount of coffee')
+# elif amount_of_coffee < smallest_item:
+#     sobra = smallest_item - amount_of_coffee
+#     print(f"Yes, I can make that amount of coffee (and even {sobra} more than that)")
+# elif amount_of_coffee > smallest_item:
+#     sobra = -(smallest_item - amount_of_coffee)
+#     print(f"No, I can make only {-(sobra - amount_of_coffee)} cups of coffee")
 # money = int(input())
 # chicken = 23
 # goat = 678

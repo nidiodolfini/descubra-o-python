9d5c5bffdf7333edec47d738c3f3c12e0e396d26
Nidio Dolfini <nidiosdolfini@gmail.com>
2020-03-11 08:38:38 -0300

    Update problem_of_the_day.py

--- jetbrains_academy/problem_of_the_day.py
+++ jetbrains_academy/problem_of_the_day.py
@@ -1,14 +1,20 @@
-import sys
+print('''"""
+THIS IS A STRING
+"""
+''')
 
-args = sys.argv
 
-# the variable "args" is already defined
-my_list = []  # your code here
-
-my_list = [int(args[1]), int(args[2]), int(args[3]), int(args[4])]
-
-print(str(my_list))
-# print(input().strip("*_~`"))
+# import sys
+#
+# args = sys.argv
+#
+# # the variable "args" is already defined
+# my_list = []  # your code here
+#
+# my_list = [int(args[1]), int(args[2]), int(args[3]), int(args[4])]
+#
+# print(str(my_list))
+# # print(input().strip("*_~`"))
 #
 
 # string = input()

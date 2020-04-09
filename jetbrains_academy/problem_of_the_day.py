31364305efd092cba9deac1bce176d282d974f8e
Nídio Dolfini <nidiosdolfini@gmail.com>
2020-04-09 13:57:19 -0300

    Update problem_of_the_day.py

--- jetbrains_academy/problem_of_the_day.py
+++ jetbrains_academy/problem_of_the_day.py
@@ -1,25 +1,4 @@
-# cafes = []
-# while True:
-#     cafe = input().split(" ")
-#     if cafe[0] == 'MEOW':
-#         cafeteira = cafes[0][0]
-#         maior = int(cafes[0][1])
-#         for i in cafes:
-#             if int(i[1]) > maior:
-#                 cafeteira = i[0]
-#         print(cafeteira)
-#         break
-#     cafes.append(cafe.copy())
-x = 1
-y = 2
-def swap():
-    global x
-    global y
-    temp = x
-    x = y
-    y = temp
-    print(x,y)
-print(x,y)
-swap()
-print(x,y)
-
+digits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+number = input()
+for i in number:
+    print(digits[int(i)])
